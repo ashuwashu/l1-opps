4174144a1bc1d427b06d8a9a794cf3aa804a4e87
ashuwashu <swetha.arun9@gmail.com>
2026-03-14 11:44:02 +0100

    Update L1oops.py

--- L1oops.py
+++ L1oops.py
@@ -10,8 +10,28 @@ class Pet():
     def enter_details(self):
         print("Enter the pet name🐾:")
         self.name=input()
+        print("Enter pet type")
+        self.type=input()
+        print("Enter pets age")
+        self.age=input()
+        print("enter the breed of the chosen animal")
+        self.breed=input()
+        print("where is your pets origin?")
+        self.origin=input()
         
 
     def show_details(self):
+        print("Pet Details")
+        print("Pet Name Is:", self.name)
+        print("Pet Type Is:", self.type)
+        print("Pet Age Is:", self.age)
+        print("Pet breed Is:", self.breed)
+        print("Pet Origin Is:", self.origin)
+
+
+
+pet1 = Pet()
+pet1.enter_details()
+pet1.show_details()
 
 
